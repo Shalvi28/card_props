98d5b34f8ff60cb1cd3a69332cfad5aadd07a86c
Shalvi28 <g1992.shalvi@gmail.com>
2022-02-06 10:05:10 +0530

    Updates

---
+++ precache-manifest.0ff0c8894a49f90db938b3e6e3e16138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68604e342d1a6a5d2624",
+    "url": "/card_props/static/css/main.6f4537cc.chunk.css"
+  },
+  {
+    "revision": "68604e342d1a6a5d2624",
+    "url": "/card_props/static/js/main.57efb6bf.chunk.js"
+  },
+  {
+    "revision": "79eb85ccd5381c4ddea1",
+    "url": "/card_props/static/js/runtime~main.d5118a25.js"
+  },
+  {
+    "revision": "0e4007777a51aae15166",
+    "url": "/card_props/static/js/2.cf2742ac.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/card_props/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0f0cb05db8b595dc0f8dbb6de88e88f9",
+    "url": "/card_props/index.html"
+  }
+];
